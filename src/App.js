7c6ff818a3f863a704941481e07a273d65942595
sowweggod <rrrrr1111111111@inbox.ru>
2024-04-12 00:27:01 +0300

    styles changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Footer from "./components/Footer"
 import './App.css';
 import Search from "./components/Search";
 import {List} from "./components/List";
+import Cover from "./components/Cover";
 
 function App() {
     return (
         <>
             <Header />
+            <Cover/>
             <Search/>
             <List/>
             <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Footer.css"
+import "./MyComponent.css"
 
 export default function Footer(){
     return (
