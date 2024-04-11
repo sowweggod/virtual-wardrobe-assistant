adccf4ab881e6bd0d6b34eef92338de7b26fa642
Violetta-MOIS <118511005+Violetta-MOIS@users.noreply.github.com>
2024-04-11 23:57:41 +0300

    gallery+regisration
    gallery+registration

---
+++ src/pages/RegistrationForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Registration from '../components/Registration';
+
+function RegistrationForm(){
+    return(
+        <>
+            <Registration/>
+        </>
+    )
+}
+
+export default Registration;
